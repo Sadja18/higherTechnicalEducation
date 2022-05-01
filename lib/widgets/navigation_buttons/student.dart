7decc25786157a539f7d2f92449f8542035b265f
Sadja18 <namanmishraec1045@gmail.com>
2022-05-01 17:00:15 +0530

    version: 0.0.0.05, Dashboard Screen created

---
+++ lib/widgets/navigation_buttons/student.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class StudentScreenNavigationButtons extends StatelessWidget {
+  const StudentScreenNavigationButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.60,
+      // height: MediaQuery.of(context).size.height * 0.16,
+      // decoration: BoxDecoration(
+      //   border: Border.all(
+      //     color: Colors.white,
+      //   ),
+      // ),
+      alignment: Alignment.topCenter,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          InkWell(
+            onTap: () {
+              if (kDebugMode) {
+                print("Navigate to attendance screen");
+              }
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.60,
+              height: MediaQuery.of(context).size.height * 0.05,
+              margin: const EdgeInsets.symmetric(
+                vertical: 4.0,
+              ),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
+              ),
+              child: const Text(
+                "Attendance",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              if (kDebugMode) {
+                print("Navigate to Time Table screen");
+              }
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.60,
+              height: MediaQuery.of(context).size.height * 0.05,
+              margin: const EdgeInsets.symmetric(
+                vertical: 4.0,
+              ),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
+              ),
+              child: const Text(
+                "Time Table",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
